4d101e0c920febcc49a29c5982ea57f51efd9e95
sathish <2000sathishkumar.d@gmail.com>
2022-12-31 12:48:32 +0530

    add script

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,17 @@
-'use strict'
+"use strict";
 
 // get elements
-const mainCardEl = document.getElementById('card-one');
-const secondCardEl = document.getElementById('card-two');
-
-
+const mainCardEl = document.getElementById("card-one");
+const secondCardEl = document.getElementById("card-two");
+const changeEl = document.getElementById("number");
+// button elements
+const buttonEl = document.querySelectorAll(".btn");
+const submitEl = document.getElementById("submit");
+const ratingEl = document.getElementById("btn-rating");
 
 // gobble variable
+function init() {
+  changeEl.innerText = 0;
+}
 
+init();
