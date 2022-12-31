61dd188f81cd0a847f485511d74f960df25ab82e
sathish <2000sathishkumar.d@gmail.com>
2022-12-31 14:37:55 +0530

    project competed

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,45 @@
 "use strict";
 
-// get elements
+// Get elements
 const mainCardEl = document.getElementById("card-one");
 const secondCardEl = document.getElementById("card-two");
 const changeEl = document.getElementById("number");
-// button elements
+// Button elements
 const buttonEl = document.querySelectorAll(".btn");
 const submitEl = document.getElementById("submit");
 const ratingEl = document.getElementById("btn-rating");
 
-// gobble variable
+// Gobble variable
 function init() {
   changeEl.innerText = 0;
 }
 
 init();
+
+// Event listener
+submitEl.addEventListener("click", function () {
+  // view second card
+  secondCardEl.classList.remove("hidden");
+  // remove main card
+  mainCardEl.style.display = "none";
+});
+
+buttonEl.forEach(function (Event) {
+  Event.addEventListener("click", function () {
+    changeEl.innerHTML = Event.innerHTML;
+  });
+});
+
+ratingEl.addEventListener("click", function () {
+  // remove second card
+  secondCardEl.classList.add("hidden");
+  // view main card
+  mainCardEl.style.display = "block";
+});
+
+// ratingEl.addEventListener("click", function () {
+//   // view main card
+//   mainCardEl.classList.remove("hidden");
+//   // remove second card
+//   secondCardEl.style.display = "none";
+// });
